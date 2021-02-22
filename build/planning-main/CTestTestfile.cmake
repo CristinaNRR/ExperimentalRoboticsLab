38c17b9f18189e6cebcc5f8eef46f2b61cad9cd1
Cristina Naso <cri.tennis97@gmail.com>
2021-02-22 15:51:16 +0100

    planning modified

---
+++ build/planning-main/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cristina/new_ws/src/planning-main
+# Build directory: /home/cristina/new_ws/build/planning-main
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
