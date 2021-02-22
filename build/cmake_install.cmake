ab3786d8495050b4cf50ab29507b7dffed5360a2
Cristina Naso <cri.tennis97@gmail.com>
2021-02-22 11:48:49 +0100

    cc

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -126,6 +126,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/cristina/new_ws/build/gtest/cmake_install.cmake")
+  include("/home/cristina/new_ws/build/exp_assignment3-main/cmake_install.cmake")
 
 endif()
 
