065a0d504720a496efc88509a8eaac45026a6a1f
Cri <cri.tennis97@gmail.com>
2021-07-01 15:38:29 +0200

    explore-lite package modified

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,10 +7,7 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/cristina/new_ws/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/cristina/new_ws/devel/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/home/cristina/new_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export LD_LIBRARY_PATH='/home/cristina/my_ros/devel/lib:/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu/gazebo-7/plugins'
+export PKG_CONFIG_PATH='/home/cristina/my_ros/devel/lib/pkgconfig:/opt/ros/kinetic/lib/pkgconfig:/opt/ros/kinetic/lib/x86_64-linux-gnu/pkgconfig'
 export PWD='/home/cristina/new_ws/build'
-export PYTHONPATH="/home/cristina/new_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/cristina/new_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
-export ROS_PACKAGE_PATH="/home/cristina/new_ws/src:$ROS_PACKAGE_PATH"
+export PYTHONPATH='/home/cristina/my_ros/devel/lib/python2.7/dist-packages:/opt/ros/kinetic/lib/python2.7/dist-packages:/home/cristina/mdk/share:/home/cristina/mdk/share'
